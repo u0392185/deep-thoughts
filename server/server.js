01e4f700b958aa004ca9961b8d4c8885d4bba6e9
Brian Anderson <andersonBrianM@gmail.com>
2022-03-19 23:20:35 -0600

    Made type defs, resolvers, and generally set up graphQL stuff.

---
+++ server/server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const { ApolloServer } = require('apollo-server-express')
+const{ typeDefs, resolvers } = require('./schemas')
+const db = require('./config/connection');
+const {
+  ApolloServerPluginLandingPageGraphQLPlayground
+} = require("apollo-server-core");
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+const startServer = async () => {
+
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    typeDefs,
+    resolvers,
+    plugins: [
+      ApolloServerPluginLandingPageGraphQLPlayground(),
+    ],
+  })
+
+  await server.start()
+
+  server.applyMiddleware({ app })
+
+  console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+}
+
+startServer()
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+db.once('open', () => {
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+  });
+});
