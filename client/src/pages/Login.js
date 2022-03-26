aa860dfcd2770a26f655a1cb693f1d63b6d390d6
Brian Anderson <andersonBrianM@gmail.com>
2022-03-25 18:18:25 -0600

    User and profile functionality.

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,7 +1,13 @@
 import React, { useState } from 'react';
 
+import { useMutation } from '@apollo/client';
+import { LOGIN_USER } from '../utils/mutations';
+import Auth from '../utils/auth'
+
 const Login = (props) => {
   const [formState, setFormState] = useState({ email: '', password: '' });
+
+  const [login, { error }] = useMutation(LOGIN_USER);
 
   // update state based on form input changes
   const handleChange = (event) => {
@@ -13,15 +19,19 @@ const Login = (props) => {
     });
   };
 
-  // submit form
-  const handleFormSubmit = async (event) => {
+  const handleFormSubmit = async event => {
     event.preventDefault();
-
-    // clear form values
-    setFormState({
-      email: '',
-      password: '',
-    });
+  
+    try {
+      const { data } = await login({
+        variables: { ...formState }
+      });
+  
+      console.log(data);
+      Auth.login(data.login.token)
+    } catch (e) {
+      console.error(e);
+    }
   };
 
   return (
@@ -53,6 +63,7 @@ const Login = (props) => {
                 Submit
               </button>
             </form>
+            {error && <div>Login failed</div>}
           </div>
         </div>
       </div>
